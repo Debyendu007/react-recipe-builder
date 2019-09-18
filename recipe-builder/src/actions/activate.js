d14d43cc1de4a6eab43f0bd745d46b0002b8e207
Debyendu007 <papai.das.011@gmail.com>
2019-09-18 10:57:19 +0530

    updated

--- recipe-builder/src/actions/activate.js
+++ recipe-builder/src/actions/activate.js
@@ -1,9 +1,8 @@
 import {ACTION_ACTIVATION} from "../action-list";
 
 const Activating = () => {
-  console.log(ACTION_ACTIVATION)
   return {
-    type: ACTION_ACTIVATION.ACTION_ACTIVATE_GADGET
+    type: ACTION_ACTIVATION.ACTIVATE_GADGET
   }
 }
 
